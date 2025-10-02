7ec74e6271bc81d43f26e4c547a654eaaef03a50
Даниил <danilka.volkov.02@mail.ru>
2025-10-02 16:54:06 +0300

    Initial commit

---
+++ extensions/directus-extension-resolve-route/src/index.ts
@@ -0,0 +1,50 @@
+import { defineEndpoint } from '@directus/extensions-sdk';
+import { matchRoute } from "./utils/match-route";
+
+async function extracted(service, req, res, exceptions) {
+	const routeService = new service('pages', {
+		schema: req.schema,
+		accountability: req.accountability
+	});
+
+	const path = Array.isArray(req.params.path) ? req.params.path : [req.params.path];
+
+	try {
+		const routes = await routeService.readByQuery({
+			fields: ['*', 'blocks.*.*']
+		});
+		const requestedPath = '/' + path.join('/');
+
+		for (const route of routes) {
+			const params = matchRoute(route.slug, requestedPath);
+
+			if (params) {
+				return res.json({
+					matched: true,
+					template: route.slug,
+					params,
+					data: route
+				});
+			}
+		}
+
+		return res.json({matched: false});
+	} catch (error) {
+		throw new exceptions.ServiceUnavailableException(error.message);
+	}
+}
+
+export default defineEndpoint({
+	id: 'resolve-route',
+	handler: (router, { services, exceptions }) => {
+		const { ItemsService } = services;
+
+		router.get('/:path*', async (req, res) => {
+			return await extracted(ItemsService, req, res, exceptions);
+		});
+
+		router.get('', async (req, res) => {
+			return await extracted(ItemsService, req, res, exceptions);
+		});
+	},
+});
